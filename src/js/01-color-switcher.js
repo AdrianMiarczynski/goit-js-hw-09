2c86790ce0a439b32c2c5f78058b7a839504ec58
Adrian Miarczyński <adrian331144@gmail.com>
2023-04-07 18:34:47 +0200

    finish first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnElStart = document.querySelector('button[data-start]');
+const btnElStop = document.querySelector('button[data-stop]');
+btnElStop.disabled = true;
 
+let interval;
+
+const onClick = () => {
+  if (!interval) {
+    interval = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+};
+const removeinterval = () => {
+  clearInterval(interval);
+  interval = null;
+};
+
+btnElStart.addEventListener('click', () => {
+  onClick();
+  btnElStart.disabled = true;
+  btnElStop.disabled = false;
+});
+
+btnElStop.addEventListener('click', () => {
+  removeinterval();
+  btnElStart.disabled = false;
+  btnElStop.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
